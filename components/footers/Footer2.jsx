4a800ec04f17ec6af2e4b919c722fe00da979944
o7digital <101292703+o7digital@users.noreply.github.com>
2025-12-01 13:51:36 -0600

    Revert "Nettoyage du footer et retrait des sections inutiles"
    This reverts commit ad4050f11dab44c474d6e77381d3fe03b47b9982.

--- components/footers/Footer2.jsx
+++ components/footers/Footer2.jsx
@@ -1,7 +1,7 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import { footerSections } from "@/data/footerLinks";
+import { footerSections, socialLinks } from "@/data/footerLinks";
 export default function Footer2() {
   return (
     <footer className="rainbow-footer footer-style-default no-border">
@@ -9,7 +9,7 @@ export default function Footer2() {
         <div className="container">
           <div className="row">
             {footerSections.map((section, index) => (
-              <div className="col-lg-3 col-md-6 col-sm-6 col-12" key={index}>
+              <div className="col-lg-2 col-md-6 col-sm-6 col-12" key={index}>
                 <div className="rainbow-footer-widget">
                   <h4 className="title">{section.title}</h4>
                   <div className="inner">
@@ -24,6 +24,52 @@ export default function Footer2() {
                 </div>
               </div>
             ))}
+            <div className="col-lg-4 col-md-6 col-sm-6 col-12">
+              <div className="rainbow-footer-widget">
+                <h4 className="title">Stay With Us.</h4>
+                <div className="inner">
+                  <h6 className="subtitle">
+                    2000+ Our clients are subscribe Around the World
+                  </h6>
+                  <ul className="social-icon social-default justify-content-start">
+                    {socialLinks.map((link, index) => (
+                      <li key={index}>
+                        <a href={link.href}>
+                          <i className={link.iconClass} />
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="newsletter-area rainbow-newsletter-default">
+        <div className="container">
+          <div className="row row--0 newsletter-wrapper align-items-center border-top-bottom">
+            <div className="col-lg-4">
+              <div className="newsletter-section-title">
+                <h3 className="title">Stay up to Date</h3>
+                <p className="description">
+                  Don't miss the latest Doob news, update and trend.
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-8">
+              <form
+                className="rainbow-newsletter mt_md--20 mt_sm--20"
+                onSubmit={(e) => e.preventDefault()}
+              >
+                <div className="form-group">
+                  <input type="email" placeholder="Email Address" />
+                </div>
+                <div className="form-group">
+                  <button className="btn-default">Subscribe</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
       </div>
--- data/footerLinks.js
+++ data/footerLinks.js
@@ -1,12 +1,30 @@
 export const socialLinks = [
-  { href: "https://www.linkedin.com/company/o7-digital", iconClass: "feather-linkedin" },
+  { href: "https://www.facebook.com/", iconClass: "feather-facebook" },
+  { href: "https://www.twitter.com", iconClass: "feather-twitter" },
+  { href: "https://www.instagram.com/", iconClass: "feather-instagram" },
+  { href: "https://www.linkdin.com/", iconClass: "feather-linkedin" },
 ];
 
 export const socialLinksWithSal = [
   {
-    href: "https://www.linkedin.com/company/o7-digital",
-    iconClass: "feather-linkedin",
+    href: "https://www.facebook.com/",
+    iconClass: "feather-facebook",
     salDelay: 200,
+  },
+  {
+    href: "https://www.twitter.com",
+    iconClass: "feather-twitter",
+    salDelay: 300,
+  },
+  {
+    href: "https://www.instagram.com/",
+    iconClass: "feather-instagram",
+    salDelay: 400,
+  },
+  {
+    href: "https://www.linkdin.com/",
+    iconClass: "feather-linkedin",
+    salDelay: 500,
   },
 ];
 
@@ -14,9 +32,37 @@ export const footerSections = [
   {
     title: "Services",
     links: [
-      { href: "/services", label: "Services" },
-      { href: "/about", label: "À propos" },
+      { href: "/about", label: "About" },
+      { href: "/portfolio", label: "Portfolio" },
       { href: "/contact", label: "Contact" },
+      { href: "/service", label: "Service" },
+    ],
+  },
+  {
+    title: "Solutions",
+    links: [
+      { href: "/brand", label: "Brand" },
+      { href: "/call-to-action", label: "Call To Action" },
+      { href: "/counter", label: "Counter" },
+      { href: "/service", label: "Service" },
+    ],
+  },
+  {
+    title: "Company",
+    links: [
+      { href: "/pricing", label: "Pricing" },
+      { href: "/tab", label: "Tab Styles" },
+      { href: "/service", label: "Service" },
+      { href: "/social-share", label: "Social" },
+    ],
+  },
+  {
+    title: "Resources",
+    links: [
+      { href: "/team", label: "Team" },
+      { href: "/testimonial", label: "Testimonial" },
+      { href: "/service", label: "Service" },
+      { href: "/timeline", label: "Timeline" },
     ],
   },
 ];
