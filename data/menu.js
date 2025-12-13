0ffb52d7fda8c7a5e1485fd328d032ce862ecc78
o7digital <101292703+o7digital@users.noreply.github.com>
2025-12-13 03:02:24 -0600

    Remove services page and point menu to portfolio

--- data/menu.js
+++ data/menu.js
@@ -1,6 +1,6 @@
 export const menuItems = [
   { title: "Accueil", href: "/index-digital-agency" },
   { title: "O7Digital", href: "/about" },
-  { title: "Services", href: "/services" },
+  { title: "Portfolio", href: "/portfolio-three-column" },
   { title: "Contact", href: "/contact" },
 ];
