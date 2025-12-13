bd1b9096155931d86850fb0d22ea12873ed63848
o7digital <101292703+o7digital@users.noreply.github.com>
2025-12-12 22:12:32 -0600

    About: remplacement section Expertise par positionnement business & direction digitale

--- components/o7/about/Expertise.jsx
+++ components/o7/about/Expertise.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import SectionHeader from "../common/SectionHeader";
-import { o7Expertise } from "@/data/o7";
 
 export default function Expertise() {
   return (
@@ -10,35 +9,35 @@ export default function Expertise() {
           <div className="col-lg-12">
             <SectionHeader
               eyebrow="Expertise"
-              title="Expertise"
+              title="Une implication au cœur du business de nos clients"
               description=""
             />
           </div>
         </div>
         <div className="row row--15 service-wrapper">
-          {o7Expertise.map((item, index) => (
-            <div
-              key={item.title}
-              className="col-lg-4 col-md-6 col-sm-6 col-12"
-              data-sal="slide-up"
-              data-sal-duration={650}
-              data-sal-delay={index * 50}
-            >
-              <div className="service service__style--1 icon-circle-style text-center">
-                <div className="icon">
-                  <i className="feather-star" />
-                </div>
-                <div className="content">
-                  <h4 className="title w-600">
-                    <a href="#">{item.title}</a>
-                  </h4>
-                  <p className="description b1 color-gray mb--0">
-                    {item.description}
-                  </p>
-                </div>
+          <div
+            className="col-lg-12"
+            data-sal="slide-up"
+            data-sal-duration={650}
+            data-sal-delay={0}
+          >
+            <div className="service service__style--1 icon-circle-style text-center">
+              <div className="icon">
+                <i className="feather-star" />
+              </div>
+              <div className="content">
+                <p className="description b1 color-gray mb--10">
+                  Chez O7 Digital Consulting, nous ne nous limitons pas à la création de sites web ou au déploiement de solutions techniques. Nous nous impliquons directement dans le cœur du business de nos clients, en agissant comme une véritable direction informatique et digitale externalisée.
+                </p>
+                <p className="description b1 color-gray mb--10">
+                  Notre rôle est d’aligner la technologie, le design et la performance digitale avec les objectifs stratégiques de l’entreprise. Nous accompagnons les dirigeants dans leurs décisions clés, structurons les outils numériques, optimisons les processus et construisons des plateformes capables de soutenir la croissance réelle du business.
+                </p>
+                <p className="description b1 color-gray mb--0">
+                  Nous travaillons en étroite collaboration avec les équipes internes, les partenaires et les décideurs afin de garantir une cohérence totale entre vision, exécution et résultats. Chaque projet est pensé comme un levier de performance, de rentabilité et de pérennité, au service des ambitions de nos clients.
+                </p>
               </div>
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
